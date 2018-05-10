cf2b32bfdd97a85a8e3bfa9f553404ab800c5be3
Alex MacKenzie <alexbcmac7@gmail.com>
2018-05-10 15:57:21 -0700

    Imports and uses router and component in App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import CreateGoal from './components/CreateGoal';
+import PatchGoal from './components/PatchGoal';
+import GetGoal from './components/GetGoal';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <Router>
+            <div className={"App"}>
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to Your Goal Tracker</h1>
+                </header>
+                <ul>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/create'}>Create</Link></li>
+                    <li><Link to={'/get'}>List</Link></li>
+                    <li><Link to={'/patch/:id'}>Edit</Link></li>
+                </ul>
+                <hr />
+                <Switch>
+                    <Route exact path='/create' component={CreateGoal} />
+                    <Route path='/get' component={GetGoal} />
+                    <Route path='/patch/:id' component={PatchGoal} />
+                </Switch>
+            </div>
+        </Router>
     );
   }
 }
